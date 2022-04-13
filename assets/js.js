88e2e12b46a9942793177daa420a815e21dddc15
nathan <codejourney2022@gmail.com>
2022-04-13 17:35:59 -0400

    Add various properties to signUp variable on event

--- assets/js.js
+++ assets/js.js
@@ -1,4 +1,7 @@
 const signUp = document.querySelector('#signUp');
 signUp.addEventListener('click', () => {
-    signUp.style.background = 'blue';
+    signUp.style.background = '#fff';
+    signUp.style.color = '#B4CFB0';
+    signUp.style.borderColor = "#B4CFB0";
+    signUp.textContent = "Thank you!";
 })
